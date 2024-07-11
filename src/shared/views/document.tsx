314f701d8faf3eb378caaaf32f4d649ab71088cf
KamilGrocholski <mikololo26@gmail.com>
2024-07-11 08:12:54 +0200

    scripts version

--- src/shared/views/document.tsx
+++ src/shared/views/document.tsx
@@ -6,16 +6,10 @@ export function Document({ children }: PropsWithChildren) {
       <head>
         <meta charset="UTF-8" />
         <title>title</title>
-        <meta
-          http-equiv="Cache-Control"
-          content="no-cache, no-store, must-revalidate"
-        />
-        <meta http-equiv="Pragma" content="no-cache" />
-        <meta http-equiv="Expires" content="0" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link href="/public/global.css" rel="stylesheet" type="text/css" />
-        <script src="/public/htmx.min.js" defer></script>
-        <script src="/public/alpine.min.js" defer></script>
+        <script src="/public/htmx_v2.0.0.js" defer></script>
+        <script src="/public/alpine_v3.14.1.js" defer></script>
       </head>
       <body>{children}</body>
     </html>
