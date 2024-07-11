1b597ff3b79021e379a78a5ac5f8b6cdc62e6b1e
KamilGrocholski <mikololo26@gmail.com>
2024-07-11 10:29:31 +0200

    add doctype to document

--- src/shared/views/document.tsx
+++ src/shared/views/document.tsx
@@ -1,17 +1,21 @@
+import { html } from "hono/html";
 import { PropsWithChildren } from "hono/jsx";
 
 export function Document({ children }: PropsWithChildren) {
   return (
-    <html lang="en">
-      <head>
-        <meta charset="UTF-8" />
-        <title>title</title>
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <link href="/public/global.css" rel="stylesheet" type="text/css" />
-        <script src="/public/htmx_v2.0.0.js" defer></script>
-        <script src="/public/alpine_v3.14.1.js" defer></script>
-      </head>
-      <body>{children}</body>
-    </html>
+    <>
+      {html`<!doctype html>`}
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8" />
+          <title>title</title>
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <link href="/public/global.css" rel="stylesheet" type="text/css" />
+          <script src="/public/htmx_v2.0.0.js" defer></script>
+          <script src="/public/alpine_v3.14.1.js" defer></script>
+        </head>
+        <body>{children}</body>
+      </html>
+    </>
   );
 }
